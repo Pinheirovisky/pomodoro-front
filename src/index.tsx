7dccc917ef474406aad10ef28b2bcdfb55140f29
Vinicius Pinheiro <vinicius.pinheiro@wisereducacao.com>
2021-05-29 13:38:35 -0300

    style: add reset styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { App } from 'containers';
 
 ReactDOM.render(
   <React.StrictMode>
