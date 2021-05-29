598932eb846fdc2471d976887adf1e6f602b05b5
Vinicius Pinheiro <vinicius.pinheiro@wisereducacao.com>
2021-05-29 13:38:48 -0300

    styles: add button styles

---
+++ src/components/Button/button.styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+interface Props {
+  working?: boolean;
+}
+
+const Wrapper = styled.button<Props>`
+  background: ${(props) => (props.working ? '#EF5D50' : '#41e1ba')};
+  border: 1px solid transparent;
+  cursor: pointer;
+  padding: 10px 25px;
+  color: ${(props) => (props.working ? '#FFF' : '#000a')};
+  transition: background-color 300ms ease-in-out;
+  margin: 20px auto;
+`;
+
+export default Wrapper;
