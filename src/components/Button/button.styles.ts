fb7ae7e4789d6a2353fbc92ad7ee45941a5dad1a
Vinicius <pinheiro.desenv@gmail.com>
2021-05-31 23:12:38 -0300

    style: add a border-radius button

--- src/components/Button/button.styles.ts
+++ src/components/Button/button.styles.ts
@@ -7,6 +7,7 @@ interface Props {
 const Wrapper = styled.button<Props>`
   background: ${(props) => (props.working ? '#EF5D50' : '#41e1ba')};
   border: 1px solid transparent;
+  border-radius: 5px;
   cursor: pointer;
   padding: 10px 25px;
   color: ${(props) => (props.working ? '#FFF' : '#000a')};
